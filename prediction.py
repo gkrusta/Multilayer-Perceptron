086370382d95d4e21933a6aee1beb1e8fc3ac98b
gkrusta <krustagabriela@gmail.com>
2025-09-27 18:19:16 +0200

    bu bu

---
+++ prediction.py
@@ -0,0 +1,16 @@
+import numpy as np
+from train import NeuronalNetwork
+from layer import Layer
+
+
+class Predict:
+    def __init__(self):
+
+
+    def main():
+        model = np.load("model.npz")
+        print(model["W1"])
+
+
+    if __name__ == "__main__":
+        main()
